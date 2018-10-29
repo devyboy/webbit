0af6e2c93532476c7c9ca37588d549958d539119
devyboy <dev@udel.edu>
2018-10-29 19:19:55 -0400

    first commit

---
+++ public/build/precache-manifest.0a495d30ac34781bb3a442de4c9ac447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c003410f75b79ccbfa39",
+    "url": "/static/js/main.c003410f.chunk.js"
+  },
+  {
+    "revision": "221128c3f2a7a4441ab4",
+    "url": "/static/js/1.221128c3.chunk.js"
+  },
+  {
+    "revision": "c003410f75b79ccbfa39",
+    "url": "/static/css/main.9907ab27.chunk.css"
+  },
+  {
+    "revision": "1ce97e2d1cbdb98617ba9c3326d7aee6",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+
+var config = {
+    apiKey: "AIzaSyC03mP6r5qEkEF_UA1eSw1pRPgQLW2nlEU",
+    authDomain: "websecrbac.firebaseapp.com",
+    databaseURL: "https://websecrbac.firebaseio.com",
+    projectId: "websecrbac",
+    storageBucket: "websecrbac.appspot.com",
+    messagingSenderId: "766124786977"
+  };
+
+firebase.initializeApp(config);
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: "",
+      password: "",
+      newPassword: "",
+      email: null,
+      error: "",
+      color: "",
+    }
+  }
+
+  signIn(user, pass) {
+    firebase.auth().signInWithEmailAndPassword(user, pass).then((result) => {
+      this.setState({ email: firebase.auth().currentUser.email, error: "" });
+    }, (error) => {
+      this.setState({ error: error.message, color: "red" })
+    });
+  }
+
+  register(user, pass) {
+    firebase.auth().createUserWithEmailAndPassword(user, pass).then((result) => {
+      this.setState({ error: "You've registered a new account and may now sign in with it.", color: "green" });
+    }, (error) => {
+      this.setState({ error: error.message, color: "red" });
+    });
+  }
+
+  logOut() {
+    firebase.auth().signOut();
+    window.location.reload();
+  }
+
+  handleUserChange(event) {
+    this.setState({ user: event.target.value })
+  }
+
+  handlePassChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleNewPassChange(event) {
+    this.setState({ newPassword: event.target.value });
+  }
+
+  changePassword() {
+    firebase.auth().currentUser.updatePassword(this.state.newPassword).then((result) => {
+      this.setState({ error: "Password successfully changed. I hope you remembered it!", color: "green" })
+    }, (error) => {
+      this.setState({ error: error.message, color: "red" })
+    });
+  }
+
+  componentWillUnmount() {
+    firebase.auth().signOut();
+  }
+
+  componentDidMount() {
+    firebase.auth().signOut();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {this.state.email
+          ?
+          <div>
+            <p> Welcome {this.state.email}! You can change your password or log out. </p>
+            <form className="form">
+              <label>
+                <input type="password" value={this.state.newPassword} onChange={this.handleNewPassChange.bind(this)} placeholder="New Password"/>
+              </label>
+            </form>
+            <div className="button-group">
+              <div
+                  className="button"
+                  onClick={() => this.changePassword()}
+              >
+                  Change Password
+              </div>
+              <div
+                  className="button"
+                  onClick={() => this.logOut()}
+              >
+                  Log Out
+              </div>
+            </div>
+          </div>
+          :
+          <div>
+          <p> Welcome to WebSec Reddit, if you have an account, please sign in. If not, please register one! </p>
+          <form className="form">
+            <label>
+              <input type="email" value={this.state.user} onChange={this.handleUserChange.bind(this)} placeholder="Email"/>
+            </label>
+            <br/>
+            <label>
+              <input type="password" value={this.state.password} onChange={this.handlePassChange.bind(this)} placeholder="Password"/>
+            </label>
+          </form>
+          <div className="button-group">
+            <div
+                className="button"
+                onClick={() => this.signIn(this.state.user, this.state.password)}
+            >
+                Sign In
+            </div>
+            <div
+                className="button"
+                onClick={() => this.register(this.state.user, this.state.password)}
+            >
+                Register
+            </div>
+          </div>
+          </div>
+        }
+        <p style={{ color: this.state.color }} className="error">{this.state.error}</p>
         </header>
       </div>
     );
