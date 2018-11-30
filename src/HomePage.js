1a23697b3c6cdd93209c9b0c692e4c80af4385d5
devyboy <dev@udel.edu>
2018-11-29 20:36:37 -0500

    new thread functionality working

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,20 +3,19 @@ import firebase from "firebase";
 import './App.css';
 import { Link } from "react-router-dom";
 import Thread from './Thread';
+import NewThread from "./NewThread";
 import Loading from "./loading.svg";
 import ReactLoading from 'react-loading';
+import { Modal, Button } from 'react-bootstrap';
 
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      threads: null,
+      threads: false,
+      open: false,
     };
-  }
-
-  handleNewPassChange(event) {
-    this.setState({ newPassword: event.target.value });
   }
 
   changePassword(e) {
@@ -29,7 +28,12 @@ class HomePage extends Component {
 
   componentDidMount() {
     firebase.database().ref("/threads/").on('value', (snapshot) => {
-      this.setState({ threads: Object.entries(snapshot.val()) });
+      if (snapshot.val() !== null) {
+        this.setState({ threads: Object.entries(snapshot.val()) });
+      }
+      else {
+        this.setState({ threads: false });
+      }
     });
   }
 
@@ -46,11 +50,19 @@ class HomePage extends Component {
     return(
         <div className="App">
           <h1 className="App-title" onClick={() => window.open("https://github.com/devyboy/websec-reddit")}>Webbit</h1>
+          <Modal style={{ color: "white" }} show={this.state.open} onHide={() => this.setState({ open: false })}>
+            <Modal.Header style={{ backgroundColor: "#3d4148" }} >
+              <Modal.Title style={{fontSize: "1.5em"}}>
+                Submit a new thread
+              </Modal.Title>
+            </Modal.Header>
+            <NewThread closeModal={() => this.setState({ open: false })} userObject={this.props.userObject} />
+          </Modal>
           {this.props.userObject 
             ? 
             <div className="App-settings">
               <Link className="account-name" to={"/settings"}> Hi {this.props.userObject.email.substring(0, this.props.userObject.email.indexOf("@"))}!</Link>
-              <Link className="new-post" to={'/new'}>New Post</Link>
+              <div className="new-post" onClick={() => this.setState({ open: true })}>New Post</div>
               <div className="sign-out" onClick={() => firebase.auth().signOut()}>
                 Logout
               </div>
@@ -64,7 +76,7 @@ class HomePage extends Component {
           }
           <header className="App-header">
             {this.state.threads
-              ? 
+              ?
               this.state.threads.map((thread, key) => {
                 return(
                   <div key={key}>
@@ -74,8 +86,9 @@ class HomePage extends Component {
                 );
               })
               :
-              <ReactLoading type={"spin"} color={"white"} height={150} width={150} />
+              <h1>There are no threads, feel free to make one!</h1>
             }
+            {this.state.threads === null && <ReactLoading type={"spin"} color={"white"} height={150} width={150} />}
           </header>
         </div>
     );
