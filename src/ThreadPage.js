68f36fd3af5a193afa87bbfb459dcc411a649975
vgutta <vineethg@udel.edu>
2018-12-01 14:32:44 -0500

    comment form only shows up when logged in

--- src/ThreadPage.js
+++ src/ThreadPage.js
@@ -106,34 +106,86 @@ class Settings extends Component {
                         </div>
                     }
                 </div>
-                <div className="App-header">
-                    {this.state.currentThread === null && 
-                        <ReactLoading type={"spin"} color={"white"} height={150} width={150} />
-                    }
-                    <div className="thread-page-container">
-                        <div className="thread-page-title">
-                            {this.state.currentThread.title}
-                            {/* <div style={{ display: "inline-block", marginLeft: '.75em', verticalAlign: "middle" }}>
-                                <div onClick={this.upVote.bind(this)} className="upvote">
-                                    <img src={this.state.upVoted ? Upvote : UpvoteGrey} style={{height: 15}} />
-                                </div>
-                                <div style={{ fontSize: ".45em" }}>{console.log(this.state.currentThread.upvotes)}</div>
-                                <div onClick={this.downVote.bind(this)} className="downvote">
-                                    <img src={this.state.downVoted ? Downvote : DownvoteGrey} style={{height: 15}} />
-                                </div>
-                            </div> */}
+                {this.props.userObject
+                    ?
+                    <div className="App-header">
+                        {this.state.currentThread === null && 
+                            <ReactLoading type={"spin"} color={"white"} height={150} width={150} />
+                        }
+                        <div className="thread-page-container">
+                            <div className="thread-page-title">
+                                {this.state.currentThread.title}
+                                {/* <div style={{ display: "inline-block", marginLeft: '.75em', verticalAlign: "middle" }}>
+                                    <div onClick={this.upVote.bind(this)} className="upvote">
+                                        <img src={this.state.upVoted ? Upvote : UpvoteGrey} style={{height: 15}} />
+                                    </div>
+                                    <div style={{ fontSize: ".45em" }}>{console.log(this.state.currentThread.upvotes)}</div>
+                                    <div onClick={this.downVote.bind(this)} className="downvote">
+                                        <img src={this.state.downVoted ? Downvote : DownvoteGrey} style={{height: 15}} />
+                                    </div>
+                                </div> */}
+                            </div>
+                            <div className="thread-page-author">
+                                {this.state.currentThread &&
+                                    <div>
+                                        Posted by {this.state.currentThread.author + " "}
+                                        <TimeAgo live={false} date={new Date(this.state.currentThread.date * 1000)} />
+                                    </div>
+                                }
+                            </div>
+                            <hr />
+                            <div className="thread-page-content">{this.state.currentThread.content}</div>
+                            <hr />
+                            <div className="comments">
+
+                            </div>
+                            <form className="add-comment">
+                                <lable>
+                                    Comment:
+                                    <input type="text" name="comment" />
+                                </lable>
+                            </form>
                         </div>
-                        <div className="thread-page-author">
-                            {this.state.currentThread &&
-                                <div>
-                                    Posted by {this.state.currentThread.author + " "}
-                                    <TimeAgo live={false} date={new Date(this.state.currentThread.date * 1000)} />
-                                </div>
-                            }
-                        </div>
-                        <hr />
-                        <div className="thread-page-content">{this.state.currentThread.content}</div>
                     </div>
+                    :
+                    <div className="App-header">
+                        {this.state.currentThread === null && 
+                            <ReactLoading type={"spin"} color={"white"} height={150} width={150} />
+                        }
+                        <div className="thread-page-container">
+                            <div className="thread-page-title">
+                                {this.state.currentThread.title}
+                                {/* <div style={{ display: "inline-block", marginLeft: '.75em', verticalAlign: "middle" }}>
+                                    <div onClick={this.upVote.bind(this)} className="upvote">
+                                        <img src={this.state.upVoted ? Upvote : UpvoteGrey} style={{height: 15}} />
+                                    </div>
+                                    <div style={{ fontSize: ".45em" }}>{console.log(this.state.currentThread.upvotes)}</div>
+                                    <div onClick={this.downVote.bind(this)} className="downvote">
+                                        <img src={this.state.downVoted ? Downvote : DownvoteGrey} style={{height: 15}} />
+                                    </div>
+                                </div> */}
+                            </div>
+                            <div className="thread-page-author">
+                                {this.state.currentThread &&
+                                    <div>
+                                        Posted by {this.state.currentThread.author + " "}
+                                        <TimeAgo live={false} date={new Date(this.state.currentThread.date * 1000)} />
+                                    </div>
+                                }
+                            </div>
+                            <hr />
+                            <div className="thread-page-content">{this.state.currentThread.content}</div>
+                            <hr />
+                            <div className="comments">
+
+                            </div>
+                        </div>
+                    </div>
+                }
+                <div className="App-bottombar">
+                    <p style={{color: "white", textAlign: "center", padding: "10px", fontSize: "15px"}}>
+                    Made by <a href="https://github.com/devyboy" target="_blank" rel="noopener noreferrer">Dev</a>, <a href="https://github.com/mbillone" target="_blank" rel="noopener noreferrer">Matt</a>, and <a href="https://github.com/vgutta" target="_blank" rel="noopener noreferrer">Vineeth</a>
+                    </p>
                 </div>
             </div>
         );
