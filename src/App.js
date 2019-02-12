920dd324af12d1003797a71a02a140f86f9c8d05
devyboy <dev@udel.edu>
2019-02-12 14:50:55 -0500

    fixed credentials

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import firebase from "firebase";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import HomePage from "./HomePage";
+import Settings from "./Settings";
+import ThreadPage from "./ThreadPage"
+import FourOhFour from "./FourOhFour";
+import './App.css';
+import { yeet } from "./config.js";
+
+var config = {
+    apiKey: yeet.apiKey,
+    authDomain: yeet.authDomain,
+    databaseURL: yeet.databaseURL,
+    projectId: yeet.projectId,
+    storageBucket: yeet.storageBucket,
+    messagingSenderId: yeet.messagingSenderId
+  };
+
+firebase.initializeApp(config);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userObject: false,
+        };
+
+        firebase.auth().onAuthStateChanged((user) => {
+            this.setState({ userObject: user });
+        });
+    }
+
+    render() {
+        return(
+            <Router>
+                <Switch>
+                        <Route exact path="/" render={(props) => <HomePage {...props} userObject={this.state.userObject} />} />
+                        <Route exact path="/home" render={(props) => <HomePage {...props} userObject={this.state.userObject} />} />
+                        <Route exact path="/home/:tid" render={(props) => <ThreadPage {...props} userObject={this.state.userObject} />} />
+                        <Route path="/login" render={(props) => <Login {...props} userObject={this.state.userObject} />} />
+                        <Route path="/settings/" render={(props) => <Settings {...props} userObject={this.state.userObject} />} />
+                        <Route component={FourOhFour} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default App;
